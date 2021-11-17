2399fe0d90f4a56b8e49d901163c181d3d293455
ironman93 <finn.striegl@gmail.com>
2021-11-17 21:40:46 +0100

    fixed layout and added rounding to telemetry

--- W Seminar 02/src/RocketLaunch.java
+++ W Seminar 02/src/RocketLaunch.java
@@ -1,7 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics2D;
 import javax.swing.*;
 
@@ -22,53 +21,45 @@ public class RocketLaunch extends StandardAnwendung implements ActionListener{
 	int thrust = 34000000;
 	double m = 2900000;
 	double h = 0;
+	double twr = 0;
 	int y_pos = 930;
 	Color bg = new Color(100,100,100);
 	Timer timer = new Timer((int)(dt*1000), this);
 	JLabel velocityLabel = new JLabel("velocity: 0");
-	JLabel heightLabel = new JLabel("distance: 0");
-	JLabel accelerationLabel = new JLabel("acceleration: 9.81");
+	JLabel heightLabel = new JLabel("height: 0");
+	JLabel accelerationLabel = new JLabel("acceleration: 0");
+	JLabel twrLabel = new JLabel("twr: 0");
 	
 	public RocketLaunch() {
 		super("titel", 250, 1000);
 		this.setBackground(bg);
-		
+		setLayout(null);
 		
 		JButton startButton = new JButton("start");
-		//add(startButton);
-		//startButton.setVisible(true);
-		startButton.addActionListener(this);
+		startButton.addActionListener(this);	
 		
-		//startButton.setMaximumSize(new Dimension(100, 50));
+		add(startButton);
+		add(velocityLabel);
+		add(heightLabel);
+		add(accelerationLabel);
+		add(twrLabel);
 		
-		GroupLayout layout = new GroupLayout(fenster.getContentPane());
-		layout.setAutoCreateGaps(true);
-		layout.setAutoCreateContainerGaps(true);
-		fenster.getContentPane().setLayout(layout);
+		startButton.setLocation(20, 10);
+		startButton.setSize(55,20);
+		velocityLabel.setLocation(20, 40);
+		velocityLabel.setSize(100,20);
+		heightLabel.setLocation(20, 60);
+		heightLabel.setSize(100,20);
+		accelerationLabel.setLocation(20, 80);
+		accelerationLabel.setSize(100,20);
+		twrLabel.setLocation(20, 100);
+		twrLabel.setSize(100, 20);
 		
-		
-		layout.setVerticalGroup(
-				layout.createSequentialGroup()
-				.addComponent(startButton)
-				.addComponent(velocityLabel)
-				.addComponent(heightLabel)
-				.addComponent(accelerationLabel)
-		);
-		
-		layout.setHorizontalGroup(
-				layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
-				.addComponent(startButton)
-				.addComponent(velocityLabel)
-				.addComponent(heightLabel)
-				.addComponent(accelerationLabel)
-		);		
-
-		setLayout(layout);
 		startButton.setVisible(true);
 		velocityLabel.setVisible(true);
 		heightLabel.setVisible(true);
 		accelerationLabel.setVisible(true);
-		
+		twrLabel.setVisible(true);
 	}
 
 	void calcAcceleration(){
@@ -87,28 +78,32 @@ public class RocketLaunch extends StandardAnwendung implements ActionListener{
 		m = m-(14000*dt);
 	}
 	
+	void calcTwr() {
+		twr = thrust/(m*10);
+	}
+	
 	void launch() {
 		calcAcceleration();
 		calcVelocity();
 		calcHeight();
 		calcMass();
+		calcTwr();
 		t = t + dt;
 		y_pos = 930 - (int) (h * 0.01);
-		//y_pos = (int)( h * 0.1);
 		updateTelemetry();
 		this.repaint();
 	}
 	
 	public void updateTelemetry() {
 		velocityLabel.setText("velocity: "+Math.round(v));
-		heightLabel.setText("hoehe: "+Math.round(h));
-		accelerationLabel.setText("acceleration: " +Math.round(a));
-		
+		heightLabel.setText("height: "+Math.round(h));
+		accelerationLabel.setText("acceleration: " + (double)Math.round(a * 100d) / 100d);
+		twrLabel.setText("twr: " + (double)Math.round(twr * 100d) / 100d);
 	}
 	
 	@Override
 	public void zeichne(Graphics2D g) {
-		g.fillOval(200, (int)y_pos, 20, 20);
+		g.fillOval(150, (int)y_pos, 20, 20);
 	}
 	
 	public void actionPerformed(ActionEvent arg0) {
